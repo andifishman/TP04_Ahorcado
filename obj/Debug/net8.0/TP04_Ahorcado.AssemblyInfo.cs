163d7a5291f4b969d2d9a416658acb8aa665f42c
ANDRÉS ILÁN FISHMAN <49435429@ort>
2025-05-21 12:12:02 -0300

    por las dudas

--- obj/Debug/net8.0/TP04_Ahorcado.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP04_Ahorcado.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP04_Ahorcado")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44bb450d7c38a3b28726068db523c593532cbc2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d21242fc20c781140e67d6170bc8d5b9536ce92")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP04_Ahorcado")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP04_Ahorcado")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
