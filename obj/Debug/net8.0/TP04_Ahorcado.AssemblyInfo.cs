b67f26b59c1017673a0b0f51b61577139e4b55ac
ANDRÉS ILÁN FISHMAN <49435429@ort>
2025-05-22 15:42:47 -0300

    terminado

--- obj/Debug/net8.0/TP04_Ahorcado.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP04_Ahorcado.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP04_Ahorcado")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+728ecca99d89b970f198997df759cdfec909ee57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37989fa2f8f4aebca7b14e34cf828c56d8bff1e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP04_Ahorcado")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP04_Ahorcado")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
